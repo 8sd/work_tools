e9b8bc1821a05c93bc2d56c66fb66eb648d5442c
Sebastian Duda <sebastian.duda@uni-bayreuth.de>
2024-01-24 15:17:19 +0100

    Add new students

--- viva_tools.py
+++ viva_tools.py
@@ -7,6 +7,8 @@
 import argparse
 import sys
 
+import pandas as pd
+
 from pathlib import Path
 from pypdf import PdfReader
 from tqdm import tqdm
@@ -70,33 +72,61 @@ def iter_students (path, exists, check):
             print(get_student_name(student))
 
 
+def neue_arbeit(student, path):
+    print(student)
+    folder_name = f"{student['name']}_({student['level']})"
+
+    # Create folder for Student
+    s_path = path / folder_name
+    s_path.mkdir()
+
+    # Create subfolders
+    (s_path / "Arbeit").mkdir()
+    (s_path / "Dateien").mkdir()
+    (s_path / "Gutachten").mkdir()
+
+
+def neue_arbeiten(file, path):
+    students = pd.read_csv(file, sep="\t", header=0)
+    print(students)
+    for _, student in students.iterrows():
+        neue_arbeit(student, path)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Viva-drive checker.')
-    parser.add_argument('--arbeit', dest='arbeit', action='store_true')
-    parser.add_argument('--gutachten', dest='gutachten', action='store_true')
-    parser.add_argument('--existiert', dest='existiert', action='store_true')
-    parser.add_argument(dest='subfolder', action='store', type=str)
+    parser.add_argument('--neue-arbeit', dest='neue_arbeit', action='store_true', required=False)
+    parser.add_argument('--file', dest='file', action='store', type=str, required=False)
+
+    parser.add_argument('--check-documents', dest='check_documents', action='store_true', required=False)
+    parser.add_argument('--arbeit', dest='arbeit', action='store_true', required=False)
+    parser.add_argument('--gutachten', dest='gutachten', action='store_true', required=False)
+    parser.add_argument('--existiert', dest='existiert', action='store_true', required=False)
+    parser.add_argument('--subfolder', dest='subfolder', action='store', type=str, required=False)
     args = parser.parse_args()
 
-    viva_path /= args.subfolder
+    if args.check_documents:
+        viva_path /= args.subfolder
 
-    if args.existiert:
-        exist = ""
-    else:
-        exist = " nicht"
+        if args.existiert:
+            exist = ""
+        else:
+            exist = " nicht"
 
-    if args.arbeit and not args.gutachten:
-        doc = "Arbeiten"
-    elif not args.arbeit and args.gutachten:
-        doc = "Gutachten"
-    else:
-        parser.print_help()
+        if args.arbeit and not args.gutachten:
+            doc = "Arbeiten"
+        elif not args.arbeit and args.gutachten:
+            doc = "Gutachten"
+        else:
+            parser.print_help()
 
-    print(f"Suche nach{exist} existierenden {doc} im Ordner {args.subfolder}.", file=sys.stderr)
+        print(f"Suche nach{exist} existierenden {doc} im Ordner {args.subfolder}.", file=sys.stderr)
 
-    if args.arbeit:
-        iter_students(viva_path, args.existiert, check_for_thesis)
-    if args.gutachten:
-        iter_students(viva_path, args.existiert, check_for_assessment)
+        if args.arbeit:
+            iter_students(viva_path, args.existiert, check_for_thesis)
+        if args.gutachten:
+            iter_students(viva_path, args.existiert, check_for_assessment)
+
+    elif args.neue_arbeit:
+        neue_arbeiten(args.file, viva_path / "01_Offen oder Laufend")
 
